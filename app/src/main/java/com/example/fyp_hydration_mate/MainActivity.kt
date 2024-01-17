57007b29a264567f86e6e62654e35d508c97ebf4
Maxamed Maxamed <20088349@mail.wit.ie>
2024-01-17 19:03:54 +0000

    second commit

---
+++ app/src/main/java/com/example/fyp_hydration_mate/MainActivity.kt
@@ -0,0 +1,18 @@
+package com.example.fyp_hydration_mate
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import androidx.recyclerview.widget.LinearLayoutManager
+import androidx.recyclerview.widget.RecyclerView
+
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+
+
+
+    }
+}
